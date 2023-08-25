bfb1ecc4646ac1210efd1aac2a94e79fe53316fe
Vishal Kushwaha <133014886+meMeowya@users.noreply.github.com>
2023-08-25 13:30:06 +0530

    Add day-5

---
+++ day-5-python-loops/loops/main.py
@@ -0,0 +1,10 @@
+# For loop for a list of items
+# fruits = ["Apple", "Pear", "Peach"]
+# for fruit in fruits:
+#   print(fruit + " " + "Pie")
+
+# For loop using range function
+sum_of_numbers = 0
+for number in range(1, 101):
+    sum_of_numbers += number
+print(sum_of_numbers)
