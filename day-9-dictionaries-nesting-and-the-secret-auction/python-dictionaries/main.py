5694e50e54bd7813bc13f71dfea7a11b8c78382f
Vishal Kushwaha <133014886+meMeowya@users.noreply.github.com>
2023-08-25 14:20:33 +0530

    Add day-9

---
+++ day-9-dictionaries-nesting-and-the-secret-auction/python-dictionaries/main.py
@@ -0,0 +1,17 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.",
+                          "Function": "A piece of code that you can easily call over and over again.",
+                          "Loop": "The action of doing something over and over again."}
+
+# Adding a new Key-Value pair to dictionary.
+# print(programming_dictionary)
+
+# Create an empty dictionary.
+empty_dictionary = {}
+
+# Wipe a dictionary clean.
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Loop through a dictionary.
+for key in programming_dictionary:
+    print(f"{key}: {programming_dictionary[key]}")
