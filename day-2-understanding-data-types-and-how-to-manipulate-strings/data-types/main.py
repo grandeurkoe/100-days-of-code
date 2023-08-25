5d9f7f7cbdf2ac70813cbc1915c432029a2c3122
Vishal Kushwaha <133014886+meMeowya@users.noreply.github.com>
2023-08-25 13:00:52 +0530

    Add day-2

---
+++ day-2-understanding-data-types-and-how-to-manipulate-strings/data-types/main.py
@@ -0,0 +1,9 @@
+# Data Types
+# "Hello"[0] - This is called Subscript
+print("Hello"[4])
+
+# Mathematical Operations
+print(3 * (3 + 3) / 3 - 3)
+
+# Rounding off to the nearest integer
+print(round(7 / 3, 2))
