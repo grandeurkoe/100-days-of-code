8e9eddf9a62032eb9de8aad3a3faad7c3f438e69
Vishal Kushwaha <133014886+meMeowya@users.noreply.github.com>
2023-08-25 13:24:11 +0530

    Add day-4

---
+++ day-4-randomisation-and-python-lists/random-numbers/main.py
@@ -0,0 +1,10 @@
+# Random Numbers
+# import random
+# random_int = random.randint(1,10)
+# random_float = random.uniform(1,5)
+# print(f"The random integer number is {random_int}.")
+# print(f"The random floating point number is {random_float}.")
+
+# List
+fruits = ["Apple", "Banana", "Orange", "Pineapple"]
+print(fruits[1])
