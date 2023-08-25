7d2a8d075c1d4baf6db54753a4332887ab07e17b
Vishal Kushwaha <133014886+meMeowya@users.noreply.github.com>
2023-08-25 12:50:58 +0530

    Add day-1

---
+++ day-1-working-with-variables-in-python-to-manage-data/printing/main.py
@@ -0,0 +1,2 @@
+print("Hello" + " Meowya")
+print("I would like to " + input("What would you like to do? "))
---
+++ day-1-working-with-variables-in-python-to-manage-data/variable/main.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+length = len(name)
+print("Your name is " + name)
+print("Your name is " + str(length) + " characters long")
