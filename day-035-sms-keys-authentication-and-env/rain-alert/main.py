d0c7befa5f47694f1ab82dea6d55e5c0bd9ce5ac
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-06 13:30:55 +0530

    feat: Send SMS when raining

--- day-035-sms-keys-authentication-and-env/rain-alert/main.py
+++ day-035-sms-keys-authentication-and-env/rain-alert/main.py
@@ -1,10 +1,18 @@
 import requests
+
+# the twilio library allows us to send SMS.
+from twilio.rest import Client
+
 # Get your own OpenWeather API by creating a free account at "https://openweathermap.org/"
 # API Keys are required to make API Requests to the OpenWeather API.
 API_KEY = "49fc8dbe79a58a9c34a6f8eb2dc65042"
 MY_LAT = 19.194550
 MY_LON = 73.190819
 is_raining = False
+
+# You need account_sid and auth_token to send SMS using the twilio library.
+account_sid = "AC4f183a5a3527be369675e890c6f08666"
+auth_token = "468ed090156868e8b62134ae0937374e"
 
 rain_param = {
     'lat': MY_LAT,
@@ -26,9 +34,17 @@ rain_data_hourly = rain_data['hourly']
 for hourly_index in range(0, 12):
     if rain_data_hourly[hourly_index]['weather'][0]['id'] < 700:
         is_raining = True
-        print("Bring Umbrella")
+        client = Client(account_sid, auth_token)
+
+        message = client.messages \
+            .create(
+            body="It's going to rain today. Remember to bring an ☔.",
+            from_='+13344686603',
+            to='+919930895622'
+        )
+
+        print(message.status)
         break
 
 if is_raining is False:
     print("Don't bring Umbrella.")
-
