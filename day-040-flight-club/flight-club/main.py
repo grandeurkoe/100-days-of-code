4585054d1611780d9ffbe139371c0a8f431f790d
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-11 18:30:27 +0530

    Add day-040

---
+++ day-040-flight-club/flight-club/main.py
@@ -0,0 +1,38 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import FlightData
+from notification_manager import NotificationManager
+from customer_acquisition import Customer
+
+DEPARTURE_CITY = "London"
+CITIES = []
+LOWEST_PRICES = []
+
+flight_sheet_data = DataManager()
+flight_search = FlightSearch()
+flight_structure_data = FlightData()
+flight_notification = NotificationManager()
+customer = Customer()
+
+sheety_data = flight_sheet_data.get_sheety_data()
+
+for each_entry in sheety_data:
+    CITIES.append(each_entry['city'])
+    LOWEST_PRICES.append(each_entry['lowestPrice'])
+
+iata_codes = flight_search.get_iata_code(CITIES, 'city')
+departure_iata_code = flight_search.get_iata_code([DEPARTURE_CITY], 'city')
+
+# flight_data.update_column(iata_codes, 'iataCode')
+
+cheapest_flights = flight_search.get_cheapest_flight(departure_iata_code, iata_codes, LOWEST_PRICES, DEPARTURE_CITY, CITIES)
+
+best_offer = flight_structure_data.get_best_offer(cheapest_flights)
+
+# flight_notification.sms_best_offer(best_offer)
+
+# customer.add_customer()
+
+flight_notification.email_best_offer(best_offer)
+
+
